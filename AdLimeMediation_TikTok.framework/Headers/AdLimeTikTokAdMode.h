88e526dacc8c00e8fd923e647a9c0338d6a3c8c1
Ham-mer <2419568879@qq.com>
2020-06-09 15:10:33 +0800

    .

--- AdLimeMediation_TikTok.framework/Headers/AdLimeTikTokAdMode.h
+++ AdLimeMediation_TikTok.framework/Headers/AdLimeTikTokAdMode.h
@@ -9,26 +9,26 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSUInteger, AdLimeTikTokBannerMode) {
-    ADLIME_TIKTOK_BANNER_NORMAL = 0, // Banner（穿山甲已弃用）
+    ADLIME_TIKTOK_BANNER_NORMAL = 0, // Banner
     ADLIME_TIKTOK_BANNER_EXPRESS = 1 // 个性化模版 Banner
 };
 
 typedef NS_ENUM(NSUInteger, AdLimeTiktokNativeMode) {
-    ADLIME_TIKTOK_NATIVE_CUSTOM_BANNER = 0, // 自渲染 Banner（穿山甲已弃用）
-    ADLIME_TIKTOK_NATIVE_CUSTOM_INTERSTITIAL = 2, // 自渲染插屏（穿山甲已弃用）
-    ADLIME_TIKTOK_NATIVE_CUSTOM_FEEDLIST = 3, // 自渲染信息流（支持海外使用 Native Api 来访问信息流）
-    ADLIME_TIKTOK_NATIVE_EXPRESS_FEEDLIST = 4 // 个性化模版信息流（支持海外使用 Native Api 来访问信息流）
+    ADLIME_TIKTOK_NATIVE_CUSTOM_BANNER = 0, // 自渲染 Banner
+    ADLIME_TIKTOK_NATIVE_CUSTOM_INTERSTITIAL = 2, // 自渲染插屏
+    ADLIME_TIKTOK_NATIVE_CUSTOM_FEEDLIST = 3, // 自渲染信息流（仅用于支持海外 AL 使用 Native Api 来访问信息流）
+    ADLIME_TIKTOK_NATIVE_EXPRESS_FEEDLIST = 4 // 个性化模版信息流（仅用于支持海外 AL 使用 Native Api 来访问信息流）
 };
 
 typedef NS_ENUM(NSUInteger, AdLimeTiktokInterstitialMode) {
-    ADLIME_TIKTOK_INTERSTITIAL_NORMAL = 0, // 插屏（穿山甲已弃用）
-    ADLIME_TIKTOK_INTERSTITIAL_FULLSCREENVIDEO = 1, // 全屏视频（穿山甲已弃用）
+    ADLIME_TIKTOK_INTERSTITIAL_NORMAL = 0, // 插屏
+    ADLIME_TIKTOK_INTERSTITIAL_FULLSCREENVIDEO = 1, // 全屏视频
     ADLIME_TIKTOK_INTERSTITIAL_EXPRESS = 2, // 个性化模版插屏
     ADLIME_TIKTOK_INTERSTITIAL_EXPRESS_FULLSCREENVIDEO = 3 // 个性化模版全屏视频
 };
 
 typedef NS_ENUM(NSUInteger, AdLimeTiktokRewardedVideoMode) {
-    ADLIME_TIKTOK_REWARDEDVIDEO_NORMAL = 0, // 激励视频（穿山甲已弃用）
+    ADLIME_TIKTOK_REWARDEDVIDEO_NORMAL = 0, // 激励视频
     ADLIME_TIKTOK_REWARDEDVIDEO_EXPRESS = 1 // 个性化模版激励视频
 };
 
@@ -38,10 +38,12 @@ typedef NS_ENUM(NSUInteger, AdLimeTiktokSplashMode) {
 };
 
 typedef NS_ENUM(NSUInteger, AdLimeTiktokFeedListMode) {
-    ADLIME_TIKTOK_FEEDLIST_CUSTOM = 0, // 自渲染信息流（穿山甲已弃用）
+    ADLIME_TIKTOK_FEEDLIST_CUSTOM = 0, // 自渲染信息流
     ADLIME_TIKTOK_FEEDLIST_EXPRESS = 1, // 个性化模版信息流
-    ADLIME_TIKTOK_FEEDLIST_DRAW = 2, // Draw 信息流（穿山甲已弃用）
-    ADLIME_TIKTOK_FEEDLIST_EXPRESS_DRAW = 3 // 个性化模版 Draw 信息流
+    ADLIME_TIKTOK_FEEDLIST_DRAW = 2, // Draw 信息流
+    ADLIME_TIKTOK_FEEDLIST_EXPRESS_DRAW = 3, // 个性化模版 Draw 信息流
+    ADLIME_TIKTOK_FEEDLIST_CUSTOM_BANNER = 4, // 自渲染 Banner
+    ADLIME_TIKTOK_FEEDLIST_CUSTOM_INTERSTITIAL = 5 // 自渲染插屏
 };
 
 typedef NS_ENUM(NSUInteger, AdLimeTiktokOrienration) {
